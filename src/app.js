011f3088609bc58b7e0c5ad9b1a74a23de333c95
emma-ax <emily19491@gmail.com>
2022-09-19 09:38:10 +0300

    Added a relevant icon

--- src/app.js
+++ src/app.js
@@ -1,17 +1,37 @@
+function displayDay(daynumber) {
+  let now = new Date();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  return `${day}/${month}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dayElement = document.querySelector("#day-today");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
-  dayElement.innerHTML = response.data.wind.speed;
+  dayElement.innerHTML = displayDay();
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "38f4ffb4599af28a6348dacfb33c46b5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Athens&appid=${apiKey}&units=metric`;
+let city = "Athens";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
