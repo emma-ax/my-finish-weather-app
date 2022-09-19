90825155095012d4b5cab038b531ef01e28b810b
emma-ax <emily19491@gmail.com>
2022-09-19 10:10:35 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -31,7 +31,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "38f4ffb4599af28a6348dacfb33c46b5";
-let city = "Athens";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "38f4ffb4599af28a6348dacfb33c46b5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityUserElement = document.querySelector("#user-city");
+  search(cityUserElement.value);
+}
+
+search("Milan");
+
+let form = document.querySelector("#input-form");
+form.addEventListener("submit", handleSubmit);
